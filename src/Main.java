fd2cb415492ff0bd290992fdb62084345e9d253b
Derek Nowicki <derek@knowikiapps.com>
2018-02-24 16:11:41 -0800

    first commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+	static Scanner in = new Scanner(System.in);
+	
+	static void printCommandMenu() {
+		System.out.println("(2) Fixed integer values 1-100");
+	}
+
+	public static void main(String[] args) {
+		printCommandMenu();
+	}
+
+}
